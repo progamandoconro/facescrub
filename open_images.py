90789e2439b31aa17d5122b476a97c199672ffbd
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-06-26 21:55:14 +0100

    Update open_images.py

--- open_images.py
+++ open_images.py
@@ -1,5 +1,7 @@
+#conda create -n my_env python=3.7
 #conda activate my_env
 #cd Downloads/download
+#python
 #######################################################
 import os
 from PIL import Image
